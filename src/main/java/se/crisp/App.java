c6f6d10683ecb4f8c0f2e17ccecc0f90d31a840a
Max Wenzin <max_wenzin@discovery.com>
2023-01-21 10:24:44 +0100

    Cleanup

--- src/main/java/se/crisp/App.java
+++ src/main/java/se/crisp/App.java
@@ -10,7 +10,7 @@ import static java.util.logging.Level.INFO;
 
 /**
  * A demo of virtual threads in Java 19
- * The objective is to have a somewhat fair comparison of the performance
+ * The objective is to have a somewhat fair comparison of the performance (throughput)
  * between a fixed thread pool and virtual threads
  * when doing blocking work (not CPU intensive work)
  */
@@ -18,12 +18,14 @@ public class App {
 
     private static final Logger LOGGER = Logger.getLogger("app");
     private static final int DEFAULT_NUMBER_OF_EXECUTIONS = 50000;
+    private static final int TIMEOUT_SECONDS = 30;
 
     public static void main(String[] args) throws InterruptedException {
         boolean virtualThreads = args.length > 0 && args[0].equalsIgnoreCase("on");
         int numberOfExecutions = args.length > 1 ? Integer.parseInt(args[1]) : DEFAULT_NUMBER_OF_EXECUTIONS;
         LOGGER.log(INFO, () -> "Using Virtual Threads: %s".formatted(virtualThreads));
         LOGGER.log(INFO, () -> "Number of executions: %s".formatted(numberOfExecutions));
+        LOGGER.log(INFO, () -> "The program will timeout in: %s seconds".formatted(TIMEOUT_SECONDS));
 
         try (ExecutorService fixedThreadPoolExecutor = Executors.newFixedThreadPool(100);
              ExecutorService virtualThreadPerTaskExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
@@ -34,20 +36,21 @@ public class App {
             Runnable runnable = createRunnable(countDownLatch);
             for (int i = 0; i < numberOfExecutions; i++) {
                 if (virtualThreads) {
-                    //Thread.startVirtualThread(runnable);
                     virtualThreadPerTaskExecutor.execute(runnable);
                 } else {
-                    //new Thread(runnable).start();
                     fixedThreadPoolExecutor.execute(runnable);
                 }
             }
 
-            boolean status = countDownLatch.await(30, TimeUnit.SECONDS);
-            long finish = System.currentTimeMillis();
-            long timeElapsed = finish - start;
-            LOGGER.log(INFO, () -> "Run time: %s, finished successfully: %s".formatted(timeElapsed, status));
+            boolean status = countDownLatch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            long timeElapsed = millisElapsedFrom(start);
+            LOGGER.log(INFO, () -> "Run time: %s ms, finished successfully: %s".formatted(timeElapsed, status));
         }
 
+    }
+
+    private static long millisElapsedFrom(long startMillis) {
+        return System.currentTimeMillis() - startMillis;
     }
 
     private static Runnable createRunnable(CountDownLatch countDownLatch) {
